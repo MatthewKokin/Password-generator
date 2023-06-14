4f2d17c7ccd3ba44b77b63ffee6c845f3d6a35ea
Tomguy03 <136599195+Tomguy03@users.noreply.github.com>
2023-06-14 16:13:30 +0100

    change 2

--- calc.py
+++ calc.py
@@ -15,7 +15,7 @@ def generate_password(length):
     all_characters = string.ascii_letters + string.digits + string.punctuation
     ourcharacters = []
     e = 0
-    # change
+    # change2
     #print(req)
     while e < len(req):
         Req = req.split()
