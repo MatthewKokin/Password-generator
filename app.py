d91c3de9451e568cf80d2b2cdad496990c82aca7
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-06-19 17:14:45 +0100

    Working prototype with GTP code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 import string
 
 app = Flask(__name__)
-app.debug = True
+app.run(debug=True)
 
 @app.route('/')
 def home():
@@ -12,11 +12,26 @@ def home():
 @app.route('/generate', methods=['POST'])
 def generate_password():
     length = request.form.get('length', type=int)
-    all_characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(all_characters) for i in range(length))
+    special_chars = request.form.get('special-characters')
+
+    all_characters = string.ascii_letters + string.digits
+    password = []
+
+    # Add special characters
+    for char in special_chars:
+        password.append(char)
+
+    # Fill the remaining length with random characters
+    remaining_length = length - len(password)
+    for _ in range(remaining_length):
+        password.append(random.choice(all_characters))
+
+    # Shuffle the list to ensure randomness
+    random.shuffle(password)
+    
+    password = ''.join(password)
+
     return render_template('index.html', password=password)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# Test
