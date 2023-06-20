739a6f0b034926150997a41b2130e7a1af441411
Tomguy03 <136599195+Tomguy03@users.noreply.github.com>
2023-06-20 11:31:27 +0100

    Create alarm.py
    alarm (work in progress)

---
+++ alarm.py
@@ -0,0 +1,39 @@
+import time
+import datetime
+from datetime import date
+from datetime import timedelta
+from datetime import datetime
+
+numb = str(input("Number of alarms"))
+
+datetimes = []
+
+i = 0
+while i < numb:
+    
+    Date = str(input("Date"))
+    Time = str(input("Time"))
+
+    if Date == "today":
+        Date = str(date.today())
+
+    elif Date == "tomorrow":
+        yesterday = date.today()
+        Date = yesterday + timedelta(days=1)
+        Date = str(Date)
+
+    datetime_string = Date + ' ' + Time
+    datetime_obj = datetime.strptime(datetime_string, '%Y-%m-%d %H:%M:%S')
+    datetimes.append(datetime_obj)
+    
+    i = i + 1
+
+while True:
+    current_time = datetime.now()
+    u = 0
+    while u < len(datetimes):
+        if current_time > datetime_obj:
+            print("alarm")
+            datetimes.remove(datetimes(u))
+            break
+    time.sleep(0.1)
